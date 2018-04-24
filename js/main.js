0438835d1d197a8b80e30357b160d74fdc3fbc62
Richie Slocum <richie.slocum@cormorantanalytics.com>
2018-04-23 20:10:41 -0700

    initial site

---
+++ js/main.js
@@ -0,0 +1,35 @@
+var mymap = L.map('map', {
+    center: [17.7858,-64.6212],
+    zoom: 16,
+    maxZoom: 24,
+    minZoom: 8,
+    detectRetina: true // detect whether the sceen is high resolution or not.
+});
+
+var mybounds = [[17.8016, -64.6],[17.7735, -64.636]]
+
+// Add Basemap
+//L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png').addTo(mymap);
+L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',{
+    minZoom: 10,
+    maxNativeZoom: 21,
+    maxZoom: 24
+}).addTo(mymap);
+
+L.tileLayer('http://research.engr.oregonstate.edu/lidar/pointcloud/20180319_USVI/googlemaps/20180325_AllBuckIsland/{z}/{x}/{y}.png',{
+    minZoom: 10,
+    maxNativeZoom: 22,
+    maxZoom: 24,
+    noWrap: true,
+    bounds: mybounds
+}).addTo(mymap);
+
+function onMapClick(e) {
+    var pixelPoint = mymap.project(e.latlng, mymap.getZoom()).floor();
+    x = Math.floor(pixelPoint.x/256);
+    y = Math.floor(pixelPoint.y/256);
+    z = mymap.getZoom();
+    console.log(x.toString() + "/" + y.toString() + "/" + z.toString());
+}
+
+mymap.on('click', onMapClick);
