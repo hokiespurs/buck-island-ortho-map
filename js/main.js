6a797610a626e85561d06a5c1129ea501fc66b31
Richie Slocum <richie.slocum@cormorantanalytics.com>
2018-04-24 15:51:06 -0700

    update buck map

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
 var mymap = L.map('map', {
     center: [17.7858,-64.6212],
-    zoom: 16,
-    maxZoom: 24,
-    minZoom: 8,
+    zoom: 15,
+    maxZoom: 23,
+    minZoom: 13,
     detectRetina: true // detect whether the sceen is high resolution or not.
 });
 
-var mybounds = [[17.8016, -64.6],[17.7735, -64.636]]
+var mybounds = [[17.8016, -64.6],[17.7735, -64.636]];
 
 // Add Basemap
 //L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png').addTo(mymap);
 L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',{
     minZoom: 10,
     maxNativeZoom: 21,
-    maxZoom: 24
+    maxZoom: 23
 }).addTo(mymap);
 
-L.tileLayer('http://research.engr.oregonstate.edu/lidar/pointcloud/20180319_USVI/googlemaps/20180325_AllBuckIsland/{z}/{x}/{y}.png',{
+L.tileLayer('http://research.engr.oregonstate.edu/lidar/pointcloud/20180319_USVI/googlemaps/20180325_BuckIslandAll/{z}/{x}/{y}.png',{
     minZoom: 10,
     maxNativeZoom: 22,
-    maxZoom: 24,
+    maxZoom: 23,
     noWrap: true,
     bounds: mybounds
 }).addTo(mymap);
